39f83a7eb4828b46572f98b34a5a9d11f03b6fd8
Kresib <kresimir.bagaric@gmail.com>
2010-12-29 01:20:20 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kraeftemessen::Application.config.secret_token = '9574682192067f978de072ecd3d74b0cd838b64b7eef828a5202be93e487b6883c4e748a19d6963d65bd910d117d9d1617c7f8a77ca4dacbdd2ae6723461f7b1'
